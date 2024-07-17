6e86b9c56cee0f455a0eafeebbd2e7686f2a7381
protoplm <protoplmz@gmail.com>
2024-07-17 19:51:27 -0400

    Large changes - added supply graph, changed averaging methods, various bug fixes

--- parse_larva.py
+++ parse_larva.py
@@ -6,6 +6,7 @@ from plotly.subplots import make_subplots
 import plotly.io as pio
 from PIL import Image
 import io
+import pandas as pd
 
 def player_info(replay_file):
     replay = sc2reader.load_replay(replay_file, load_map=False)
@@ -20,19 +21,24 @@ def player_info(replay_file):
 
     return players_info
 
+
 def is_zvz(player_info_list):
     if len(player_info_list) == 2 and all(player['race'] == 'Zerg' for player in player_info_list):
         return True
     else:
         return False
 
+
 def frame_to_realtime(event_frame):
     return event_frame / 22.4
 
+
 def process_replay(replay_file, player_index=None):
     replay = sc2reader.load_replay(replay_file, load_map=True)
-    larva_born_dict = {}
+    larva_born_dict = {}    
     larva_died_dict = {}
+    supply_data = {}
+    max_supply_data = {}
     player_name = None
 
     if player_index is not None:
@@ -41,67 +47,27 @@ def process_replay(replay_file, player_index=None):
         for player in replay.players:
             if player.play_race == 'Zerg':
                 player_name = player.name
+                player_index = player.pid
                 break
 
     for event in replay.events:
         if isinstance(event, sc2reader.events.tracker.UnitBornEvent) and event.unit_type_name == 'Larva':
-            if player_index is not None:
-                if event.control_pid == replay.players[player_index - 1].pid:
-                    larva_born_dict[event.unit_id] = frame_to_realtime(event.frame)
-            else:
+            if event.control_pid == replay.players[player_index - 1].pid:
                 larva_born_dict[event.unit_id] = frame_to_realtime(event.frame)
 
-        elif isinstance(event, sc2reader.events.tracker.UnitDiedEvent) and event.unit_id in larva_born_dict:
+        elif isinstance(event, sc2reader.events.tracker.UnitTypeChangeEvent) and event.unit_id in larva_born_dict and event.unit_type_name == 'Egg':
             larva_died_dict[event.unit_id] = frame_to_realtime(event.frame)
 
-    return larva_born_dict, larva_died_dict, player_name
-    
+        elif isinstance(event, sc2reader.events.tracker.UnitDiedEvent) and event.unit_id in larva_born_dict and event.unit_id not in larva_died_dict:
+            larva_died_dict[event.unit_id] = frame_to_realtime(event.frame)
 
+        if event.name == 'PlayerStatsEvent' and event.player.pid == replay.players[player_index - 1].pid:
+            time_in_seconds = frame_to_realtime(event.frame)
+            supply_data[time_in_seconds] = event.food_used
+            max_supply_data[time_in_seconds] = event.food_made
 
+    return larva_born_dict, larva_died_dict, player_name, supply_data, max_supply_data
 
-def average_larva_lifespan_per_interval(born_dict, died_dict, interval_length=60):
-    average_lifespan_per_interval = {}
-    max_time = max(died_dict.values(), default=0)
-    for start_time in range(0, int(max_time) + interval_length, interval_length):
-        end_time = start_time + interval_length
-        total_lifespan = 0
-        larva_count = 0
-        for unit_id, born_time in born_dict.items():
-            if start_time <= born_time < end_time:
-                died_time = died_dict.get(unit_id, float('inf'))
-                if died_time > born_time:
-                    lifespan = min(died_time, end_time) - born_time
-                    total_lifespan += lifespan
-                    larva_count += 1
-        average_lifespan = (total_lifespan / larva_count) if larva_count else 0
-        average_lifespan_per_interval[start_time // 60] = average_lifespan
-    return average_lifespan_per_interval
-
-def average_idle_larva_per_phase(born_dict, died_dict, start_minute, end_minute, idle_threshold):
-    idle_larva_count_per_interval = {}
-    max_time = max(died_dict.values(), default=0)
-    
-    for current_time in range(int(start_minute * 60), int(min(end_minute * 60, max_time)) + 1):
-        idle_larva_count = 0
-        for unit_id, born_time in born_dict.items():
-            died_time = died_dict.get(unit_id, float('inf'))
-            if born_time + idle_threshold <= current_time <= died_time:
-                idle_larva_count += 1
-
-        current_minute = current_time // 60
-        idle_larva_count_per_interval[current_minute] = idle_larva_count_per_interval.get(current_minute, 0) + idle_larva_count
-
-    for minute in idle_larva_count_per_interval:
-        idle_larva_count_per_interval[minute] /= 60
-
-    return idle_larva_count_per_interval
-
-
-def calculate_idle_larva_counts(born_dict, died_dict, phase_intervals):
-    idle_counts = {}
-    for phase, (start_minute, end_minute, idle_threshold) in phase_intervals.items():
-        idle_counts[phase] = average_idle_larva_per_phase(born_dict, died_dict, start_minute, end_minute, idle_threshold)
-    return idle_counts
 
 def calculate_cumulative_total_larva(born_dict):
     larva_count = {}
@@ -117,126 +83,167 @@ def calculate_cumulative_total_larva(born_dict):
             cumulative_count += 1
             time_index += 1
         
-        larva_count[minute_marker // 60] = cumulative_count
+        larva_count[minute_marker // 1] = cumulative_count
 
-        minute_marker += 60
+        minute_marker += 1
     
     return larva_count
 
 
-def plotly_create_figure(benchmark_data, comparison_data, title, y_axis_label, benchmark_name=None, comparison_name=None):
+def aggregate_larva_lifespans(born_dict, died_dict, interval_length=15):
+    lifespans_per_interval = {}
+    max_time = max(died_dict.values(), default=0)
+    
+    for current_time in range(0, int(max_time) + interval_length, interval_length):
+        total_lifespan = 0
+        larva_count = 0
+        for unit_id, born_time in born_dict.items():
+            if current_time <= born_time < current_time + interval_length:
+                died_time = died_dict.get(unit_id, float('inf'))
+                if died_time > born_time:
+                    lifespan = died_time - born_time
+                    total_lifespan += lifespan
+                    larva_count += 1
+        average_lifespan = (total_lifespan / larva_count) if larva_count else 0
+        lifespans_per_interval[current_time] = average_lifespan
+
+    return lifespans_per_interval
+
+
+def calculate_idle_larva_counts(born_dict, died_dict, start_second, end_second, idle_threshold):
+    idle_larva_count_per_second = {}
+    max_time = max(died_dict.values(), default=0)
+    
+    for current_time in range(start_second, int(min(end_second, max_time)) + 1):
+        idle_larva_count = 0
+        for unit_id, born_time in born_dict.items():
+            died_time = died_dict.get(unit_id, float('inf'))
+            if born_time + idle_threshold <= current_time <= died_time:
+                idle_larva_count += 1
+        idle_larva_count_per_second[current_time] = idle_larva_count
+
+    return idle_larva_count_per_second
+
+
+def idle_larva_per_phase(born_dict, died_dict, phase_intervals):
+    idle_counts = {}
+    for phase, (start_minute, end_minute, idle_threshold) in phase_intervals.items():
+        start_second = int(start_minute * 60)
+        end_second = int(end_minute * 60)
+        idle_counts[phase] = calculate_idle_larva_counts(born_dict, died_dict, start_second, end_second, idle_threshold)
+    return idle_counts
+
+
+def plotly_create_figure(benchmark_data, comparison_data, title, y_axis_label, benchmark_name=None, comparison_name=None, window=None, x_range=None, y_range=None):
     traces = []
     colors = ['LightSkyBlue', 'LightCoral']
 
-    traces.append(go.Scatter(
-        x=list(benchmark_data.keys()),
-        y=list(benchmark_data.values()),
-        mode='lines+markers',
-        name=benchmark_name if benchmark_name else 'Benchmark',
-        marker=dict(size=8, line=dict(width=2), color=colors[0]),
-        line=dict(width=2, color=colors[0])
-    ))
+    def create_trace(data, name, color, window=None):
+        series = pd.Series(data)
+        if window:
+            series = series.rolling(window=window).mean()
+        return go.Scatter(
+            x=series.index / 60,
+            y=series.values,
+            mode='lines',
+            name=name,
+            line=dict(width=2, color=color)
+        )
 
-    if comparison_data is not None and comparison_data != benchmark_data:
-        traces.append(go.Scatter(
-            x=list(comparison_data.keys()),
-            y=list(comparison_data.values()),
-            mode='lines+markers',
-            name=comparison_name if comparison_name else 'Comparison',
-            marker=dict(size=8, line=dict(width=2), color=colors[1]),
-            line=dict(width=2, color=colors[1])
-        ))
+    traces.append(create_trace(benchmark_data, benchmark_name, colors[0], window))
+    if comparison_data:
+        traces.append(create_trace(comparison_data, comparison_name, colors[1], window))
 
     layout = go.Layout(
         title=title,
-        titlefont=dict(color='White',size=30),
+        titlefont=dict(color='White', size=30),
         paper_bgcolor='Black',
         plot_bgcolor='Black',
         xaxis=dict(
             title='Time (minutes)',
-            titlefont=dict(color='White',size=20),
-            tickfont=dict(color='White',size=16),
+            titlefont=dict(color='White', size=20),
+            tickfont=dict(color='White', size=16),
             showgrid=True,
             gridwidth=1,
             gridcolor='Gray',
+            range=[0, x_range] if x_range else None
         ),
         yaxis=dict(
             title=y_axis_label,
-            titlefont=dict(color='White',size=20),
-            tickfont=dict(color='White',size=16),
+            titlefont=dict(color='White', size=20),
+            tickfont=dict(color='White', size=16),
             showgrid=True,
             gridwidth=1,
             gridcolor='Gray',
+            range=[0, y_range] if y_range else None
         ),
         legend=dict(
             x=0,
             y=1,
             borderwidth=1,
             bgcolor='Black',
-            font=dict(color='White', size=26)  #Increased legend font size
+            font=dict(color='White', size=26)
         ),
         height=750,
         width=1400
     )
     
     fig = go.Figure(data=traces, layout=layout)
-
-    #Conditionally show or hide the legend based on the number of replays
     fig.update_layout(showlegend=len(traces) > 1)
 
     return fig
 
-def remove_last_entry(data_dict):
-    if data_dict:
-        sorted_keys = sorted(data_dict.keys())
-        last_key = sorted_keys[-1]
-        del data_dict[last_key]
-    return data_dict
 
-def all_in(comparison_replay, benchmark_replay=None,player=None, benchmark_player=None):
-
-    #ensures that calls from !compare when mixing non-zvz and zvz replays do not call the incorrect player index. A workaround to stop adding more complexity to the already cumbersome state logic of context{}
+def all_in(comparison_replay, benchmark_replay=None, player=None, benchmark_player=None):
     if player == 'not_zvz':
         player = None
     if benchmark_player == 'not_zvz':
-        player = None
+        benchmark_player = None
 
     if benchmark_replay is None:
         benchmark_replay = comparison_replay
         if player is not None:
             benchmark_player = player
-        
-    if player:
-        benchmark_born, benchmark_died, benchmark_name = process_replay(benchmark_replay, benchmark_player)
-        comparison_born, comparison_died, comparison_name = process_replay(comparison_replay, player)        
-    else:
-        benchmark_born, benchmark_died, benchmark_name = process_replay(benchmark_replay)
-        comparison_born, comparison_died, comparison_name = process_replay(comparison_replay)
 
-    benchmark_avg_lifespan = remove_last_entry(average_larva_lifespan_per_interval(benchmark_born, benchmark_died))
-    comparison_avg_lifespan = remove_last_entry(average_larva_lifespan_per_interval(comparison_born, comparison_died))
+    if player is not None:
+        benchmark_born, benchmark_died, benchmark_name, benchmark_supply, benchmark_max_supply = process_replay(benchmark_replay, benchmark_player)
+        comparison_born, comparison_died, comparison_name, comparison_supply, comparison_max_supply = process_replay(comparison_replay, player)        
+    else:
+        benchmark_born, benchmark_died, benchmark_name, benchmark_supply, benchmark_max_supply = process_replay(benchmark_replay)
+        comparison_born, comparison_died, comparison_name, comparison_supply, comparison_max_supply = process_replay(comparison_replay)
+
+    if benchmark_name == comparison_name and benchmark_born != comparison_born:
+        benchmark_name += f" ({benchmark_replay})"
+        comparison_name += f" ({comparison_replay})"
+
+    benchmark_avg_lifespan = aggregate_larva_lifespans(benchmark_born, benchmark_died, interval_length=15)
+    comparison_avg_lifespan = aggregate_larva_lifespans(comparison_born, comparison_died, interval_length=15)
 
     phase_intervals = {
-        'Early Game': (0, 7, 5),
-        'Late Game': (7, 15, 15)
+        'Early Game': (0, 7, 5)
     }
 
-    benchmark_idle_counts = calculate_idle_larva_counts(benchmark_born, benchmark_died, phase_intervals)
-    comparison_idle_counts = calculate_idle_larva_counts(comparison_born, comparison_died, phase_intervals)
+    benchmark_idle_counts = idle_larva_per_phase(benchmark_born, benchmark_died, phase_intervals)
+    comparison_idle_counts = idle_larva_per_phase(comparison_born, comparison_died, phase_intervals)
+
+    benchmark_length = max(benchmark_died.values(), default=0) / 60
+    comparison_length = max(comparison_died.values(), default=0) / 60
+    x_range = min(benchmark_length, comparison_length)
 
     figures = []
-    figures.append(plotly_create_figure(benchmark_avg_lifespan, comparison_avg_lifespan, 'Average Larva Lifespan Per Minute Comparison', 'Average Lifespan (seconds)',benchmark_name,comparison_name))
+    figures.append(plotly_create_figure(benchmark_avg_lifespan, comparison_avg_lifespan, 'Average Larva Lifespan Per Minute Comparison', 'Average Lifespan (seconds)', benchmark_name, comparison_name, x_range=x_range, y_range=30))
     
     early_game_title = 'Average Idle Larva During Early Game: >5 Second idle time'
-    late_game_title = 'Average Idle Larva During Late Game: >15 Second Idle Time'
-    
-    figures.append(plotly_create_figure(benchmark_idle_counts['Early Game'], comparison_idle_counts['Early Game'], early_game_title, 'Average Idle Larva Count',benchmark_name,comparison_name))
-    figures.append(plotly_create_figure(benchmark_idle_counts['Late Game'], comparison_idle_counts['Late Game'], late_game_title, 'Average Idle Larva Count',benchmark_name,comparison_name))
+    figures.append(plotly_create_figure(benchmark_idle_counts['Early Game'], comparison_idle_counts['Early Game'], early_game_title, 'Average Idle Larva Count', benchmark_name, comparison_name, window=15, x_range=min(7, x_range)))
 
     benchmark_total_count = calculate_cumulative_total_larva(benchmark_born)
     comparison_total_count = calculate_cumulative_total_larva(comparison_born)
+    x_range_seconds = min(max(benchmark_total_count.keys(), default=0), max(comparison_total_count.keys(), default=0)) / 60
 
-    figures.append(plotly_create_figure(benchmark_total_count,comparison_total_count,'Total Larva Spawned','Larva Count',benchmark_name, comparison_name))
+    figures.append(plotly_create_figure(benchmark_supply, benchmark_max_supply, 'Benchmark Supply Over Time', 'Supply Count', f"{benchmark_name} - Current Supply", f"{benchmark_name} - Max Supply", x_range=x_range, y_range=200))
+    figures.append(plotly_create_figure(comparison_supply, comparison_max_supply, 'Comparison Supply Over Time', 'Supply Count', f"{comparison_name} - Current Supply", f"{comparison_name} - Max Supply", x_range=x_range, y_range=200))
+    
+    figures.append(plotly_create_figure(benchmark_total_count, comparison_total_count, 'Total Larva Spawned', 'Larva Count', benchmark_name, comparison_name, x_range=x_range_seconds))
 
     imgs = [Image.open(io.BytesIO(fig.to_image(format="png"))) for fig in figures]
 
@@ -265,4 +272,3 @@ def all_in(comparison_replay, benchmark_replay=None,player=None, benchmark_playe
     image_bytes.seek(0)
 
     return image_bytes.getvalue()
-
--- thelurker.py
+++ thelurker.py
@@ -8,7 +8,7 @@ import plotly.graph_objs as go
 import parse_larva
 import io
 
-# Authorization Key: ##
+
 
 reaction_contexts = {}
 
@@ -37,88 +37,6 @@ class CustomHelpCommand(commands.HelpCommand):
 bot.help_command = CustomHelpCommand()
 
 
-
-@bot.command(name='analyze', help="Use the analyze command to view metrics from a single replay")
-async def analyze_replay(ctx):
-    if ctx.message.attachments:
-        if len(ctx.message.attachments) == 1:
-            attachment = ctx.message.attachments[0]
-            if attachment.filename.lower().endswith('.sc2replay'):
-                file_path = f'./{attachment.filename}'
-                await attachment.save(file_path)
-                players = parse_larva.player_info(file_path)
-                if parse_larva.is_zvz(players):
-                    embed = discord.Embed(
-                        title="ZvZ Detected",
-                        description="This is a ZvZ, please select which player to analyze by reacting to this message:",
-                        color=discord.Color.dark_purple()
-                    )
-                    for idx, player in enumerate(players, start=1):
-                        embed.add_field(name=f"Player {idx}", value=f"React with {idx}️⃣ to analyze {player['name']}.", inline=False)
-                    embed.set_footer(text="React below to choose a player")
-                    message = await ctx.send(embed=embed)
-
-                    # React with the numbers 1 and 2
-                    for idx in range(1, len(players) + 1):
-                        await message.add_reaction(f"{idx}️⃣")
-                    # Store the context using user.id
-                    reaction_contexts[ctx.author.id] = {
-                        "message_id": message.id,  # one message id for analyze
-                        "replay_file": file_path,
-                        "players": players,
-                        "type": "analyze"
-                    }
-                else:
-                    image_bytes = parse_larva.all_in(file_path)
-                    file = discord.File(fp=io.BytesIO(image_bytes), filename='image.png')
-                    await ctx.send(file=file)
-            else:
-                await ctx.send('Please attach a valid StarCraft II replay file with the .SC2Replay extension.')
-        elif len(ctx.message.attachments) == 2:
-            await ctx.send('Wrong command, please use !compare')
-        elif len(ctx.message.attachments) > 2:
-            await ctx.send('Please attach only one replay file for the analyze command.')
-    else:
-        await ctx.send('Else condition triggered: message has no attachments')       
-
-@bot.event
-async def on_reaction_add(reaction, user):
-    if user != bot.user and user.id in reaction_contexts:
-        context = reaction_contexts[user.id]
-
-@bot.event
-async def on_reaction_add(reaction, user):
-    if user == bot.user:
-        return
-
-    context = reaction_contexts.get(user.id)
-    print(reaction_contexts)
-    if context["type"] == "compare" and reaction.message.id in context.get("message_ids", []):
-        replay_index = context["message_ids"].index(reaction.message.id)
-        player_index = int(str(reaction.emoji)[0]) - 1
-        context["selections"][replay_index] = player_index
-
-        if None not in context["selections"]:
-            comparison_replay, benchmark_replay = context["replay_files"]
-            player_selection, benchmark_selection = [
-                        (s + 1) if isinstance(s, int) else s for s in context["selections"]] # If value is an integer, add one to adjust for 1 based index of the replay (player 1 vs player2), if it's another value, it keeps its value.
-
-            # Call all_in with the structured data
-            image_bytes = parse_larva.all_in(comparison_replay, benchmark_replay, player_selection, benchmark_selection)
-            file = discord.File(fp=io.BytesIO(image_bytes), filename='comparison.png')
-            await reaction.message.channel.send(file=file)
-            del reaction_contexts[user.id]
-        
-    if context["type"] == "analyze" and reaction.message.id == context['message_id']:
-            player_index = int(str(reaction.emoji)[0]) - 1
-            selected_player = context["players"][player_index]
-            image_bytes = parse_larva.all_in(context["replay_file"], player=selected_player["index"])
-            file = discord.File(fp=io.BytesIO(image_bytes), filename='analysis.png')
-            await reaction.message.channel.send(file=file)
-            del reaction_contexts[user.id]
-    else:
-        return # Unrelated reaction
-
 @bot.command(name='compare', help="Use the compare command to compare metrics between two replays")
 async def compare_replays(ctx):
     if len(ctx.message.attachments) != 2:
@@ -141,7 +59,6 @@ async def compare_replays(ctx):
     zvz_checks = [parse_larva.is_zvz(players) for players in players_list]
     message_ids = []
 
-    # Handle ZvZ replays
     for i, (is_zvz, players) in enumerate(zip(zvz_checks, players_list)):
         if is_zvz:
             embed = discord.Embed(
@@ -163,14 +80,113 @@ async def compare_replays(ctx):
             "message_ids": message_ids,
             "replay_files": file_paths,
             "players_list": players_list,
-            "selections": [None if is_zvz else 'not_zvz' for is_zvz in zvz_checks],  # non zvz games passed to all_in will get assigned to None in that function
+            "selections": [None if is_zvz else 'not_zvz' for is_zvz in zvz_checks],
             "type": "compare"
         }
+        print(f"Context set for user {ctx.author.id}: {reaction_contexts[ctx.author.id]}")
     else:
         # Directly process if no ZvZ matches
         image_bytes = parse_larva.all_in(file_paths[0], file_paths[1])
         file = discord.File(fp=io.BytesIO(image_bytes), filename='comparison.png')
         await ctx.send(file=file)
+
+@bot.command(name='analyze', help="Use the analyze command to view metrics from a single replay")
+async def analyze_replay(ctx):
+    if ctx.message.attachments:
+        if len(ctx.message.attachments) == 1:
+            attachment = ctx.message.attachments[0]
+            if attachment.filename.lower().endswith('.sc2replay'):
+                file_path = f'./{attachment.filename}'
+                await attachment.save(file_path)
+                players = parse_larva.player_info(file_path)
+                if parse_larva.is_zvz(players):
+                    embed = discord.Embed(
+                        title="ZvZ Detected",
+                        description="This is a ZvZ, please select which player to analyze by reacting to this message:",
+                        color=discord.Color.dark_purple()
+                    )
+                    for idx, player in enumerate(players, start=1):
+                        embed.add_field(name=f"Player {idx}", value=f"React with {idx}️⃣ to analyze {player['name']}.", inline=False)
+                    embed.set_footer(text="React below to choose a player")
+                    message = await ctx.send(embed=embed)
+
+                    for idx in range(1, len(players) + 1):
+                        await message.add_reaction(f"{idx}️⃣")
+
+                    reaction_contexts[ctx.author.id] = {
+                        "message_id": message.id,  # one message id for analyze
+                        "replay_file": file_path,
+                        "players": players,
+                        "type": "analyze"
+                    }
+                else:
+                    image_bytes = parse_larva.all_in(file_path)
+                    file = discord.File(fp=io.BytesIO(image_bytes), filename='image.png')
+                    await ctx.send(file=file)
+            else:
+                await ctx.send('Please attach a valid StarCraft II replay file with the .SC2Replay extension.')
+        elif len(ctx.message.attachments) == 2:
+            await ctx.send('Wrong command, please use !compare')
+        elif len(ctx.message.attachments) > 2:
+            await ctx.send('Please attach only one replay file for the analyze command.')
+    else:
+        await ctx.send('Else condition triggered: message has no attachments')
+
+
+@bot.event
+async def on_reaction_add(reaction, user):
+    if user == bot.user:
+        return
+
+    context = reaction_contexts.get(user.id)
+    if not context:
+        return
+
+    print(f"Processing reaction for context: {context}")
+
+    if context["type"] == "compare" and reaction.message.id in context.get("message_ids", []):
+        replay_index = context["message_ids"].index(reaction.message.id)
+        try:
+            player_index = int(str(reaction.emoji)[0]) - 1
+
+            if parse_larva.is_zvz(context["players_list"][replay_index]):
+                context["selections"][replay_index] = player_index
+            else:
+                context["selections"][replay_index] = 'not_zvz'
+
+            for i, (is_zvz, selection) in enumerate(zip([parse_larva.is_zvz(players) for players in context["players_list"]], context["selections"])):
+                if not is_zvz and selection is None:
+                    context["selections"][i] = 'not_zvz'
+
+            print(f"Updated context selections: {context['selections']}")
+
+            if all(sel is not None for sel in context["selections"]):
+                comparison_replay, benchmark_replay = context["replay_files"]
+                player_selection, benchmark_selection = [
+                    (s + 1) if isinstance(s, int) else s for s in context["selections"]
+                ]
+
+                print(f"all_in called with {comparison_replay} {benchmark_replay} {player_selection} {benchmark_selection}")
+                image_bytes = parse_larva.all_in(comparison_replay, benchmark_replay, player_selection, benchmark_selection)
+                file = discord.File(fp=io.BytesIO(image_bytes), filename='comparison.png')
+                await reaction.message.channel.send(file=file)
+                del reaction_contexts[user.id]
+        except ValueError:
+            print("Invalid reaction received, ignoring.")
+
+    elif context["type"] == "analyze" and reaction.message.id == context['message_id']:
+        try:
+            player_index = int(str(reaction.emoji)[0]) - 1
+            selected_player = context["players"][player_index]
+
+            print(f"all_in called with {context['replay_file']} player={selected_player['index']}")
+            image_bytes = parse_larva.all_in(context["replay_file"], player=selected_player["index"] + 1)
+            file = discord.File(fp=io.BytesIO(image_bytes), filename='analysis.png')
+            await reaction.message.channel.send(file=file)
+            del reaction_contexts[user.id]
+        except ValueError:
+            print("Invalid reaction received, ignoring.")
+
 
 bot.run(TOKEN)
 
